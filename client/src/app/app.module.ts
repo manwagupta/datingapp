e2996921c48ee95a011306e4da690dae8db8a556
manwagupta <gupta_manoj@hotmail>
2021-02-02 17:35:18 +0530

    Staging changes to work on another machine

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberlistComponent } from './members/memberlist/memberlist.component';
 import { MemberdetailComponent } from './members/memberdetail/memberdetail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrosComponent } from './Errors/test-erros/test-erros.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
@@ -50,7 +48,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     BrowserAnimationsModule,
     FormsModule,
     SharedModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_Services/account.service';
 
@@ -10,20 +11,37 @@ import { AccountService } from '../_Services/account.service';
 export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter()
+  registerForms: FormGroup;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.InitializeForm();
+  }
+
+  InitializeForm(){
+    this.registerForms =  new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')])
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value? null : {isMatching : true}
+    }
   }
 
   regsiter() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error)
-    });
+    console.log(this.registerForms.value);
+    // this.accountService.register(this.model).subscribe(response => {
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error)
+    // });
   }
 
   cancel() {
